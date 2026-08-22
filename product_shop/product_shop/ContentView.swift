f8338819ee6e8283f01f0692a53ead303fd5012f
Ceyhun <rzayev-ceyhun-99@mail.ru>
2026-08-22 15:48:45 +0400

    init

---
+++ product_shop/product_shop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  product_shop
+//
+//  Created by Ceyhun Rzayev on 22.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ product_shop/product_shop/product_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  product_shopApp.swift
+//  product_shop
+//
+//  Created by Ceyhun Rzayev on 22.08.26.
+//
+
+import SwiftUI
+
+@main
+struct product_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
